4dd62183e7f1f24dd34210376130769412f9436b
samifriedrich <sami.r.friedrich@gmail.com>
2022-01-31 13:19:08 -0800

    Restructured routes to support browser display vs user playlist options

--- flaskapp/app/routes.py
+++ flaskapp/app/routes.py
@@ -3,22 +3,25 @@ from flask import Flask, render_template, redirect, request, make_response, sess
 import pandas as pd
 import spotipy
 import spotipy.util as util
+from spotipy.oauth2 import SpotifyClientCredentials
 import os
 import requests
 import datetime
 import time
-import urllib.parse
 from fuzzywuzzy import fuzz, process
 from math import ceil as round_up
 
 ## Globals
 app.secret_key = os.urandom(12).hex()
+#session['uid'] = uuid.uuid4()
 ## Set API authorization keys
 # Songkick API
 sk_api_key = os.environ.get('SONGKICK_API_KEY')
 # set Spotify API authorization vars and scope
 CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_ID')
 CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET')
+CLIENT_USERNAME = os.environ.get('SPOTIFY_USERNAME')
+SQ_USERNAME = "31t6b6anet3tfqfl2vaq2wvtjwzi"
 SCOPE = 'playlist-modify-public'
 # REDIRECT_URL must be listed on ShowQuester dashboard on Spotify for Developers
 REDIRECT_URI = "http://127.0.0.1:5000/callback"
@@ -27,66 +30,31 @@ SHOW_DIALOG = True
 
 # This will become first
 # setting a single venue for testing
-venue_name = "Emo's"
-venue_url = "https://www.emosaustin.com/"
-venue_city = "Austin"
+venue_name = "Mississippi Studios"
+venue_url = "https://mississippistudios.com/"
+venue_city = "Portland"
 # will need to make state conditional, see jupyter notebook
-venue_state = "TX"
+venue_id = '5776'
 
 
 ## Routes
-@app.route('/playlist/create')
-def index():
-    return "Welcome to ShowQuester. Go to http://127.0.0.1:5000/auth?playlist_id=123456 endpoint to begin."
-
-# Authorization-code-flow Step 1.
-# ShowQuester requests permission from user.
-# User is taken to Spotify login page via auth_url
-# User logs into Spotify to authorize access
-@app.route("/auth")
-def verify():
-    session["playlist_id"] = request.args.get("playlist_id")
-    auth_url = f'{API_BASE}/authorize?client_id={CLIENT_ID}&response_type=code&redirect_uri={REDIRECT_URI}&scope={SCOPE}&show_dialog={SHOW_DIALOG}'
-    return redirect(auth_url)
 
 # "Landing page" endpoint for flask app
 @app.route('/index')
 @app.route('/')
 def index():
-    #return '<iframe src="https://open.spotify.com/embed/track/7BoVAJ0HuKcBBRmUGlzX6o" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'
     return "Welcome to ShowQuester. Go to http://127.0.0.1:5000/auth?playlist_id=123456 endpoint to begin."
-
-# authorization-code-flow Step 2.
-# Showquester requests refresh and access tokens;
-# Spotify returns access and refresh tokens
-@app.route('/callback')
-def callback():
-    #return session["playlist_id"]
-    code = request.args.get('code')
-    auth_token_url = f"{API_BASE}/api/token"
-    response = requests.post(auth_token_url, data={
-                "grant_type":"authorization_code",
-                "code":code,
-                "redirect_uri":REDIRECT_URI,
-                "client_id":CLIENT_ID,
-                "client_secret":CLIENT_SECRET
-                })
-    response_body = response.json()
-    #print(f'\nRESPONSE: {response.json()}\n')
-    session['token_info'] = response_body
-    session["access_token"] = response_body.get("access_token")
-    return redirect('go')
-
-@app.route('/go')
-def go():
-    return "Spotify user granted ShowQuester access"
 
 @app.route("/venue", methods=['GET'])
 def venue():
     venue_name = request.args.get('name')
+    #venue_name = "Mississipp Studios"  # uncomment for testing
     venue_city = request.args.get('location')
-    venue_id = get_venue_id(venue_name, venue_city)
-    if venue_id:
+    #venue_city = "Portland"   # uncomment for testing 
+    venue_info = get_venue_id(venue_name, venue_city)
+    if venue_info:
+        session['venue_info'] = venue_info
+        print(session['venue_info']['venue_name'])
         return {
             'success': True,
             'data': {
@@ -105,89 +73,77 @@ def venue():
             }
         }
 
-
-# Authorization-code-flow Step 3.
-# Use the access token to access the Spotify Web API;
-# Spotify returns requested data
-@app.route("/playlist/save", methods=['POST'])
+# Create tracklist for venue       
+@app.route("/playlist/create")
 def create():
-    venue_id = request.json.get('venue_id')
-    access_token = request.json.get('access_token')
-    global sp
-    sp = spotipy.Spotify(auth=access_token)
-
-    # get username from current user after auth
-    username = sp.me()['id']
-    print(f'\nSpotify User: {username}')
-    global USERNAME
-    USERNAME = username
-
-    print("\n>>> ShowQuester started.")
-    print(f"*****************{venue_name}*******************")
-    #venue_id = get_venue_id(venue_name, venue_city)
-
+    venue_id = session['venue_info']['venue_id']
+    #venue_id = '5776'  # Mississippi Studios
+    # Client Credentials flow via spotipy (no user login required)
+    client_credentials_manager = SpotifyClientCredentials()
+    sp_cc = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
     if venue_id:
         event_list = get_venue_events(venue_id)
         shows = events_df(event_list)
         artist_list = list(shows.artist)
-        # remove exact duplicates from list of artists
         artist_list = list(process.dedupe(artist_list, threshold=99, scorer=fuzz.token_sort_ratio))
-
-        my_playlists = get_my_public_playlists(USERNAME)
-        # search playlists for a SoundQuester venue playlist
-        venue_playlist = [(name, uri) for name, uri in my_playlists.items() if venue_name in name]
-
-        if venue_playlist:
-            playlist_name, playlist_uri = venue_playlist[0]
-            print(f'Found ShowQuester playlist for {venue_name} named "{playlist_name}"')
-        else:
-        # if venue playlist missing, create new SoundQuester playlist
-            print(f'No playlist found for "{venue_name}"')
-            playlist_name, playlist_uri = create_sq_playlist(venue_name, venue_city, venue_state)
-
-        # derive playlist_id from playlist_uri
-        playlist_id = playlist_uri.split(':')[2]
-
-        # retrieve all artist objects for performing artists
         artist_obj = []
-        print("...SEARCHING Spotify for ARTISTS...")
         for artist in artist_list:
-            artist_obj.append(get_artist(artist))
-
-        # pull one top track per artist to be added to playlist
-        tracks_to_add = []
-        print("...SEARCHING Spotify for TOP TRACKS...")
+            artist_obj.append(get_artist(artist, sp=sp_cc))
+        pl_tracks = []
         for artist in artist_obj:
             if artist is not None:
                 artist_uri = artist['uri']
-                track = get_top_track(artist_uri)
-                tracks_to_add.append(track)
-
-        # filter out empty strings where no track was found
-        tracks_to_add = list(filter(None, tracks_to_add))
-        # batch tracks into 100's to respect Spotify Web API limits
-        track_batches = list(chunks(tracks_to_add, 100))
+                track = get_top_track(artist_uri, sp=sp_cc)
+                pl_tracks.append(track)
+        pl_tracks = list(filter(None, pl_tracks))  # filter out empty strings where no track was found
+        display_tracks = pl_tracks[0:100] # trim to 100
+        session["display_tracks"] = display_tracks
+        session["playlist_tracks"] = pl_tracks
+    return 'Track list generated.'
         
-        # split into playlist creation and writing to account
+# Authorization-code-flow Step 1.
+# ShowQuester requests permission from user.
+# User is taken to Spotify login page via auth_url
+# User logs into Spotify to authorize access
+@app.route("/auth")
+def verify():
+    auth_url = f'{API_BASE}/authorize?client_id={CLIENT_ID}&response_type=code&redirect_uri={REDIRECT_URI}&scope={SCOPE}&show_dialog={SHOW_DIALOG}'
+    return redirect(auth_url)
 
-        print('...UPDATING SHOWQUESTER PLAYLIST TRACKS...')
-        for batch_num, track_uris in enumerate(track_batches):
-            if batch_num == 0:
-                result = sp.user_playlist_replace_tracks(USERNAME, playlist_id, track_uris)
-            else:
-                results = sp.user_playlist_add_tracks(USERNAME, playlist_id, track_uris)
+# authorization-code-flow Step 2.
+# Showquester requests refresh and access tokens;
+# Spotify returns access and refresh tokens
+@app.route('/callback')
+def callback():
+    #return session["playlist_id"]
+    code = request.args.get('code')
+    auth_token_url = f"{API_BASE}/api/token"
+    response = requests.post(auth_token_url, data={
+                "grant_type":"authorization_code",
+                "code":code,
+                "redirect_uri":REDIRECT_URI,
+                "client_id":CLIENT_ID,
+                "client_secret":CLIENT_SECRET
+                })
+    response_body = response.json()
+    session['token_info'] = response_body
+    session["access_token"] = response_body.get("access_token")
+    print(session)
+    return 'Logged in. Go to /playlist/save endpoint to test save.'
 
-        print('...UPDATING SHOWQUESTER PLAYLIST DESCRIPTION...')
-        playlist_descr = build_playlist_description(venue_name, venue_url, venue_city, venue_state)
-        results = update_playlist_details(playlist_id, playlist_name, playlist_descr)
-
-        print(f"COMPLETED: {venue_name}\n\n")
-
-    return "Done"
-
-
+# Authorization-code-flow Step 3.
+# Use the access token to access the Spotify Web API;
+# Spotify returns requested data
+@app.route('/playlist/save')
+def save_playlist():
+    sp = spotipy.Spotify(auth=session['access_token'])
+    results = create_sq_playlist(sp) 
+    print(results)
+    return "Playlist saved to user's account."
+        
 ## Functions
 # Checks to see if token is valid and gets a new token if not
+# CURRENTLY NOT UTILIZED BY AUTH FLOW
 def get_token(session):
     token_valid = False
     token_info = session.get("token_info", {})
@@ -215,19 +171,22 @@ def get_venue_id(venue_name, venue_city):
     To handle duplicate venue names, this function will check that venue's city matches that entered in venues.csv"""
     req = f'https://api.songkick.com/api/3.0/search/venues.json?query={venue_name}&apikey={sk_api_key}'
     response = requests.get(req)
-    print(response.json())
     num_results = response.json()['resultsPage']['totalEntries']
-
     if num_results > 0:
         results = response.json()['resultsPage']['results']['venue']
         for hit in results:
-            result_state = hit['city']['state']['displayName']
             result_city = hit['city']['displayName']
             if result_city == venue_city:
                 venue_id = hit['id']
                 sk_venue_name = hit['displayName']
-                print(f'Found {sk_venue_name} in {result_city}, {result_state} on Songkick')
-                return venue_id
+                venue_url = hit['website']
+                print(f'Found {sk_venue_name} in {result_city} on Songkick')
+                venue_info = {'venue_id': venue_id,
+                              'venue_name': sk_venue_name,
+                              'venue_city': result_city,
+                              'venue_url' : venue_url
+                }
+                return venue_info
             else:
                 continue
     else:
@@ -273,11 +232,11 @@ def events_df(event_list):
 
     return pd.DataFrame(data={'artist':artists, 'date':dates, 'artist_id':ids})
 
-def get_my_public_playlists(username):
+def get_my_public_playlists(sp):
     """Returns a dictionary of public playlist names (keys) and their uri's (values) for the given username."""
     my_playlists = {}
+    username = sp.me()['id']
     results = sp.user_playlists(username)
-
     while results:
         for playlist in results['items']:
             if playlist['public']:
@@ -288,15 +247,7 @@ def get_my_public_playlists(username):
             results = None
     return my_playlists
 
-def create_sq_playlist(venue_name, venue_city, venue_state):
-    """Create an empty Showquester playlist on Spotify for a given venue"""
-    playlist_name = f"ShowQuester: {venue_name} ({venue_city}, {venue_state})"
-    results = sp.user_playlist_create(USERNAME, playlist_name, public=True)
-    playlist_uri = results['uri']
-    print(f'Created playlist "{playlist_name}"')
-    return [playlist_name, playlist_uri]
-
-def get_artist(search_str):
+def get_artist(search_str, sp):
     """Search for an artist on Spotify and return artist object if found.
     Uses fuzzywuzzy's process.extractOne convenience function to parse search results for best match."""
     results = sp.search(search_str,type='artist')
@@ -315,7 +266,7 @@ def get_artist(search_str):
         print(f'\t\"{search_str}\" was not found on Spotify.')
         return None
 
-def get_top_track(artist_uri):
+def get_top_track(artist_uri, sp):
     """Return top track uri for a given artist on Spotify."""
     results = sp.artist_top_tracks(artist_uri)
     top_tracks = results['tracks']
@@ -341,19 +292,47 @@ def chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
+        
+def create_sq_playlist(sp):
+    """Create an empty Showquester playlist on Spotify for a given venue"""
+    username = sp.me()['id']
+    venue_name = session['venue_info']['venue_name']
+    venue_city = session['venue_info']['venue_city']
+    playlist_name = f"ShowQuester: {venue_name} ({venue_city})"
+    results = sp.user_playlist_create(username, playlist_name, public=True)
+    playlist_uri = results['uri']
+    playlist_id = playlist_uri.split(':')[2]
+    results = add_tracks(sp, playlist_id)
+    playlist_descr = build_playlist_description(venue_name, venue_city)
+    results = update_playlist_details(playlist_id, playlist_name, playlist_descr, sp)
+    print(f'Created playlist "{playlist_name}"')
+    return [playlist_name, playlist_uri]
+        
+def add_tracks(sp, playlist_id):
+        pl_tracks = session["playlist_tracks"]
+        print(pl_tracks)
+        track_batches = list(chunks(pl_tracks, 100))
+        username = sp.me()['id']
+        for batch_num, track_uris in enumerate(track_batches):
+            if batch_num == 0:
+                results = sp.user_playlist_replace_tracks(username, playlist_id, track_uris)
+            else:
+                results = sp.user_playlist_add_tracks(username, playlist_id, track_uris)
+        return results
 
-def build_playlist_description(venue_name, venue_url, venue_city, venue_state):
+def build_playlist_description(venue_name, venue_city):
     """Create description for ShowQuester playlist."""
     todays_date = datetime.date.today()
-    descr = f"A programmatically-generated playlist featuring artists coming soon to {venue_name} in {venue_city}, {venue_state}. Updated {todays_date}. Ordered by date, with this week up top and events farther in the future at the bottom. Go to {venue_url} for more info."
+    descr = f"A programmatically-generated playlist featuring artists coming soon to {venue_name} in {venue_city}. Updated {todays_date}. Ordered by date, with this week up top and events farther in the future at the bottom."
     return descr
 
-def update_playlist_details(playlist_id, playlist_name, playlist_descr):
+def update_playlist_details(playlist_id, playlist_name, playlist_descr, sp):
     """Updates playlist details.
     NOTE: There are several reports of issues when updating playlist descriptions in the Spotify community.
     Currently, it seems the only solution is to wait for the server to update, which could take a day."""
+    username = sp.me()['id']
     results = sp.user_playlist_change_details(
-            USERNAME, playlist_id=playlist_id, name=playlist_name, description=playlist_descr)
+            username, playlist_id=playlist_id, name=playlist_name, description=playlist_descr)
     #print(f'Updated playlist "{playlist_name}"')
     return results
 
